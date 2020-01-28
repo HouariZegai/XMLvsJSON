5859e3091d77d8bcad6f07af2db56bef59cb49ec
HouariZegai <houarizegai14@gmail.com>
2020-01-29 00:13:49 +0100

    Adding XML parser with his model

---
+++ src/main/java/com/houarizegai/jsonvsxml/xml/XMLParserSAX.java
@@ -0,0 +1,37 @@
+package com.houarizegai.jsonvsxml.xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import com.houarizegai.jsonvsxml.model.Employee;
+import org.xml.sax.SAXException;
+
+public class XMLParserSAX {
+
+    public static List<Employee> parseXML(String filePath) {
+        List<Employee> empList = null;
+
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            MyHandler handler = new MyHandler();
+            saxParser.parse(new File(filePath), handler);
+            //Get Employees list
+            empList = handler.getEmpList();
+            //print employee information
+            for(Employee emp : empList)
+                System.out.println(emp);
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return empList;
+    }
+
+}
