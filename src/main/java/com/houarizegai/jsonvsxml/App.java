c9a796047f6f86e44487b9a0da8f1ae18a4f4d19
HouariZegai <houarizegai14@gmail.com>
2020-01-29 00:14:40 +0100

    First test app

---
+++ src/main/java/com/houarizegai/jsonvsxml/App.java
@@ -0,0 +1,31 @@
+package com.houarizegai.jsonvsxml;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    public static Stage stage;
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+            stage.setScene(new Scene(root));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+        stage.setTitle("XML vs JSON");
+        App.stage = stage;
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
