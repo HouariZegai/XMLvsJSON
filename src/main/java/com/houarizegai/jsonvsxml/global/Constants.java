558deaddc223ca83cc68521dccf9ffa6c4e9ecad
HouariZegai <houarizegai14@gmail.com>
2020-01-29 00:14:16 +0100

    Start with back-end

---
+++ src/main/java/com/houarizegai/jsonvsxml/global/Constants.java
@@ -0,0 +1,5 @@
+package com.houarizegai.jsonvsxml.global;
+
+public class Constants {
+    public static final String DATA_FOLDER_PATH = "C:\\Users\\Houari\\Documents\\Learn\\Projects\\Java\\JavaFX\\JSONXMLComparaison\\data\\employees.xml";
+}
